509c4d661596a617a04d01ef88199bde9c52e6df
MarwaYoussef1 <marwa.abdelrahmaan@gmail.com>
2024-07-09 08:57:46 +0200

    [9.7.2024]

---
+++ Test Suites/Demo suite frames.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo suite frames</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>85fc4fbf-a1df-489e-a628-bbdc0d9c418d</testSuiteGuid>
+   <testCaseLink>
+      <guid>23b43b8a-02da-460f-b0ab-d8e2f85a5fbc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo tests/Test12_FramesDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c2ff4b18-a5b1-4ead-b2ca-c009632f9691</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo tests/Test8_DataDriven</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3707628e-e84b-4568-b037-b36bbbf5ef1b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d611aea8-8ff7-4f19-a4ee-94f27a590b7a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
